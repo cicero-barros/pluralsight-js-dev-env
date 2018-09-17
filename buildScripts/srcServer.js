8ddcf42fafb7ebd0318a7bb9c28bdf5868ebe1fc
Cicero Barros <cicero.barros@quickks.com>
2018-09-17 14:54:39 -0300

    Before production build

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
@@ -15,6 +17,14 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function (req, res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email":"bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Norton", "email":"tnorton@gmail.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Lee", "email":"lee.tina@gmail.com"}
+  ]);
 });
 
 app.listen(port, function (err) {
